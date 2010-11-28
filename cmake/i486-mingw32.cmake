19068e294176ac2bd32aa9560e5284b7301f174d
Jan Kokemüller <jan.kokemueller@gmail.com>
2010-11-28 12:53:03 +0100

    replace MinGW toolchain file

--- cmake/i486-mingw32.cmake
+++ cmake/i486-mingw32.cmake
@@ -1,29 +1,18 @@
-#!/bin/sh
-#
-# Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Thomas Cort <linuxgeek@gmail.com>
-#
-# This file is part of TinyPoker.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Based on http://www.cmake.org/Wiki/CmakeMingw
 
-set (CMAKE_SYSTEM_NAME Windows)
-set (CMAKE_C_COMPILER i486-mingw32-gcc)
-set (CMAKE_CXX_COMPILER i486-mingw32-g++)
-set (CMAKE_FIND_ROOT_PATH /usr/i486-mingw32/)
-set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set (CMAKE_INSTALL_PREFIX /usr/i486-mingw32/)
-set (RUN_TESTS_WITH_WINE 1)
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Windows)
 
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER i486-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER i486-mingw32-g++)
+
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH  /usr/i486-mingw32)
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
